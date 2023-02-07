95fdbb3f633f5adf89b523bf0d9a1aa1cadb4bbb
Sara El Araby <selaraby@banquemisr.com>
2023-02-07 10:21:19 +0200

    add controllers

---
+++ eMovies/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using eMovies.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eMovies.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var data = await _context.Movies.ToListAsync();
+            return View();
+        }
+    }
+}
